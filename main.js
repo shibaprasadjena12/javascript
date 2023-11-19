44394901a2c95e5b709f24d9a3736b8024ef8be3
shibaprasad jena <102553029+shibaprasadjena12@users.noreply.github.com>
2023-11-19 15:26:21 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+let myobj = [
+    {
+        name: "shiba",
+        age: 25,
+        dept: "devops",
+        ismarried: true
+    },
+    {
+        name: "jeena",
+        age: 25,
+        dept: "dev",
+        ismarried: true
+    },
+    {
+        name: "lona",
+        age: 22,
+        dept: "mech",
+        ismarried: false
+    },
+    {
+        name: "Pinku",
+        age: 23,
+        dept: "eee",
+        ismarried: false
+    }
+
+
+]
+
+if(myobj[0].ismarried)
+{
+    console.log(myobj[0].name)
+}
+else{
+    console.log("kichhi nahin")
+}
